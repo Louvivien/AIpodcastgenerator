9a2d07d9acd4fd181c76538755f5e7638732d0a9
YousraMashkoor <yousra.mashkoor@neurallab.io>
2023-08-04 22:25:30 +0500

    fixing urls

--- client/src/Pages/PodcastGenerator/podcastGenerator.jsx
+++ client/src/Pages/PodcastGenerator/podcastGenerator.jsx
@@ -28,7 +28,7 @@ const spkData = {
 
 function VoiceSelect({onChange, field_name}) {
 
-  const url = "http://127.0.0.1:5000/voices";
+  const url = "voices";
   // const url = process.env.REACT_APP_BASE_URL + 'voices';
   const [voices, setVoices] = useState([]);
 
@@ -79,7 +79,7 @@ function PodcastGenerator() {
   }
 
   const podcastGenerateHandler = () => {
-    const url = "http://127.0.0.1:5000/send_content";
+    const url = "send_content";
     // const url = process.env.REACT_APP_BASE_URL + 'send_content';
 
 
